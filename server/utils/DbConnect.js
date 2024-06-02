3477b27bb3a4eb15b6b8c812b6ec25d9b679fa57
Surya <thesuryapratap2003@gmail.com>
2024-06-02 13:24:05 +0530

    server env fixed

--- server/utils/DbConnect.js
+++ server/utils/DbConnect.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const mongoConnect = async () => {
   try {
     await mongoose
-      .connect(process.env.MONGO_STRING, { dbName: "SocketChatApp" })
+      .connect(process.env.MONGO_STRING || "mock_key", { dbName: "SocketChatApp" })
       .then(() => {
         console.log("Connected to db");
       });
